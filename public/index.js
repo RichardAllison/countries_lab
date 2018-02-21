740af54b1de239aa00952151cc61ba805e48c1bf
Ross Hill <rosshill88@gmail.com>
2018-02-21 14:13:40 +0000

    Can display information of select countries

--- public/index.js
+++ public/index.js
@@ -2,9 +2,9 @@
 const app = function(){
   const url = "https://restcountries.eu/rest/v2/all"
   makeRequest(url, requestComplete);
+
 }
 // -------------------
-
 
 const makeRequest = function(url, callback) {
   const request = new XMLHttpRequest();
@@ -23,11 +23,36 @@ const requestComplete = function(){
 const populateList = function(countries){
   const select = document.getElementById("country-list");
 
-  countries.forEach(function(country){
+
+  countries.forEach(function(country, index){
     const option = document.createElement("option");
     option.innerText = country.name;
+    option.value = index;
     select.appendChild(option);
   })
+
+  select.addEventListener('change', function(){
+    displayCountryInfo(countries)});
+}
+
+const displayCountryInfo = function(countries) {
+  const index = document.getElementById('country-list').value;
+  const country = countries[index];
+
+  const countrySection = document.getElementById("country-info");
+
+  const name = document.createElement("h2");
+  name.innerText = country.name;
+  countrySection.appendChild(name);
+
+  const population = document.createElement("p");
+  population.innerText = country.population;
+  countrySection.appendChild(population);
+
+  const capital = document.createElement("p");
+  capital.innerText = country.capital;
+  countrySection.appendChild(capital);
+
 }
 
 
